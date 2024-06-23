c22ff9e1073bdaa9a9e932704428762f14d8c270
Baden Glass <badenglass@gmail.com>
2024-06-23 15:58:45 -0500

    intial commit

---
+++ .config/sketchybar/plugins/darkmode.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+source $HOME/.config/sketchybar/icons.sh
+
+CONFIG="$HOME/.config/alacritty/themes/active.toml"
+THEME_DIR="$HOME/.config/alacritty/themes"
+LIGHT_THEME="$THEME_DIR/ayu_light.toml"
+DARK_THEME="$THEME_DIR/ayu_dark.toml"
+
+if [[ $(defaults read -g AppleInterfaceStyle 2>/dev/null) == "Dark" ]]
+then
+    sketchybar -m --set appearance icon="$SUN_ICN"
+    cp $DARK_THEME $CONFIG
+
+else
+    sketchybar -m --set appearance icon="$MOON_ICN"
+    cp $LIGHT_THEME $CONFIG
+fi
